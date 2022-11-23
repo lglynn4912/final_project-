da44322717284f534131c0c9ea21d6739a52fe82
Lauren Glynn <lauren.glynn@gmail.com>
2022-11-22 19:18:01 -0800

    created crud file and drink data file - need to fill with accurate data

--- model.py
+++ model.py
@@ -14,30 +14,54 @@ class User(db.Model):
     user_id = db.Column(db.Integer,
                         autoincrement=True,
                         primary_key=True)
+    user_name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
 
-    preferred_orders = db.relationship("Preferred Orders", back_populates="user")
+    # preferred_orders = db.relationship("Preferred Orders", back_populates="user")
 
     def __repr__(self):
-        return f'<User user_id={self.user_id} email={self.email}>'
+        return f"<User user_id={self.user_id} email={self.email}>"
 
 
-class PreferredOrder(db.Model):
-    """An order made by a user"""
+# class PreferredOrder(db.Model):
+#     """An order made by a user"""
 
-    __tablename__ = 'preferred-orders'
+#     __tablename__ = 'preferred-orders'
 
-    preferred_order_id = db.Column(db.Integer,
-                        autoincrement=True,
+#     preferred_order_id = db.Column(db.Integer,
+#                         autoincrement=True,
+#                         primary_key=True)
+#     preferred_order_name = db.Column(db.String, unique=False, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+
+
+#     # user = db.relationship("User", back_populates="preferred-orders")
+
+#     def __repr__(self):
+#         return f"<>"
+
+
+class MilkTypes(db.Model):
+    """Milk types"""
+
+    __tablename__ = 'milktypes'
+
+    milk_id = db.Column(db.Integer,
+                        autoincrement=False,
                         primary_key=True)
-    order_name = db.Column(db.String, unique=False, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+    milk_name = db.Column(db.String, unique=True, nullable=False)
 
-    user = db.relationship("User", back_populates="preferred-orders")
 
-    def __repr__(self):
-        return f'<Rating rating_id={self.rating_id} score={self.score}>'
+class DrinkOptions(db.Model):
+    """Standard drink names"""
+
+    __tablename__ = 'drinknames'
+
+    drink_options_id = db.Column(db.Integer,
+                        autoincrement=False,
+                        primary_key=True)
+    drink_opions_name = db.Column(db.String, unique=True, nullable=False)
 
 
 def connect_to_db(flask_app, db_uri="postgresql:///preferredorders", echo=True):
@@ -53,9 +77,5 @@ def connect_to_db(flask_app, db_uri="postgresql:///preferredorders", echo=True):
 
 if __name__ == "__main__":
     from server import app
-
-    # Call connect_to_db(app, echo=False) if your program output gets
-    # too annoying; this will tell SQLAlchemy not to print out every
-    # query it executes.
 
     connect_to_db(app)
---
+++ seed_database.py
@@ -0,0 +1,40 @@
+"""Script to seed database."""
+
+import os
+import json
+from random import choice, randint
+from datetime import datetime
+
+import crud
+import model
+import server
+
+os.system("dropdb ratings")
+os.system('createdb ratings')
+
+model.connect_to_db(server.app)
+model.db.create_all()
+
+with open('data/drink_names.json') as f:
+    order_data = json.loads(f.read())
+
+drinks_in_db = []
+
+for drink in drink_names:
+    name = drink['name']
+    drink_description = drink['description']
+   
+
+    drink = crud.create_drink(
+        name=name,
+        drink_description=drink_description 
+    )
+    
+    drinks_in_db.append(drink)
+
+
+model.db.session.add_all(drinks_in_db)
+model.db.session.commit()
+
+
+
